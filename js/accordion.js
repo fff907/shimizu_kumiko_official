431df160b84ec8cc663c56dc52eea444b2ed76a8
fff907 <59164817+fff907@users.noreply.github.com>
2025-09-05 16:09:36 +0900

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -6,6 +6,8 @@
   buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
       const content = btn.nextElementSibling;
+      // クリックされたボタンの「すぐ下の兄弟要素」を取得（＝.works-content）
+      // nextElementSibling＝「自分の次の兄弟要素」を取得するプロパティ
       content.classList.toggle('is-open');
     });
   });
